14626edc3c569de3cfb27fad30f29a0972ab0506
Vaselek <burzhuas@gmail.com>
2019-05-25 14:32:01 +0600

    Update photos

--- app/photos.js
+++ app/photos.js
@@ -25,10 +25,10 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     if (req.query.author)
-        return Photo.find({user: new ObjectId(req.query.author)})
+        return Photo.find({user: new ObjectId(req.query.author)}).populate('user')
             .then(photos => res.send(photos))
             .catch(() => res.sendStatus(500))
-    Photo.find()
+    Photo.find().populate('user')
         .then(photos => res.send(photos))
         .catch(() => res.sendStatus(500))
 });
@@ -42,9 +42,10 @@ router.get('/:id', (req, res) => {
         .catch(()=>res.sendStatus(500))
 });
 
-router.post('/', upload.single('image'), (req, res) => {
+router.post('/', auth, upload.single('image'), (req, res) => {
     const photoData = {
         title: req.body.title,
+        user: req.user
     };
     if (req.file) {
         photoData.image = req.file.filename;
@@ -52,25 +53,17 @@ router.post('/', upload.single('image'), (req, res) => {
     const photo = new Photo(photoData);
     photo.save()
         .then(result => res.send(result))
-        .catch((error) => res.sendStatus(400).send(error))
+        .catch((error) => {
+            res.sendStatus(400).send(error)
+        })
 });
 
 router.delete('/:id', auth, (req, res) => {
     Photo.findById(req.params.id)
         .then(photo => {
-            if (!photo.user.equal(req.user._id)) res.sendStatus(401)
+            if (!photo.user.equals(req.user._id)) res.sendStatus(401)
             photo.delete();
             return res.send({message: 'Deleted'});
-        })
-        .catch(()=>res.sendStatus(500))
-});
-
-router.post('/:id/publish', auth, (req, res) => {
-    Photo.findById(req.params.id)
-        .then(photo => {
-            photo.published = true;
-            photo.save();
-            return res.send({message: 'Published'});
         })
         .catch(()=>res.sendStatus(500))
 });

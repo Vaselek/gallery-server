dc2d076102c1c73112812c1a94fb2c3412196d1a
Vaselek <burzhuas@gmail.com>
2019-05-25 11:21:01 +0600

    Created facebook app and added configs to the app

---
+++ config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const rootPath = __dirname;
+
+module.exports = {
+    rootPath,
+    uploadPath: path.join(rootPath, 'public/uploads'),
+    mongoOptions: {useNewUrlParser: true},
+    dbUrl: 'mongodb://localhost/cocktails',
+    facebook: {
+        appId: '471260170082980',
+        appSecret: 'bb8c3c3622c5c67ef1aad15c9f8a50e3'
+    }
+}

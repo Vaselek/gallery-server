05677bea81b86c1a5e3924f21a42d9b302fd4cac
Vaselek <burzhuas@gmail.com>
2019-05-25 14:32:18 +0600

    Refactor db naming

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
     rootPath,
     uploadPath: path.join(rootPath, 'public/uploads'),
     mongoOptions: {useNewUrlParser: true},
-    dbUrl: 'mongodb://localhost/cocktails',
+    dbUrl: 'mongodb://localhost/gallery',
     facebook: {
         appId: '471260170082980',
         appSecret: 'bb8c3c3622c5c67ef1aad15c9f8a50e3'
